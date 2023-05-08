6a31c98b5853a0d502fb4b39731d10c087eec5b3
saravanan-09 <saravanan.v.0009@gmail.com>
2023-05-08 17:51:46 +0530

    newProject code

---
+++ demoProject/src/main/java/saravanan/demoProject/demoCode.java
@@ -0,0 +1,24 @@
+package saravanan.demoProject;
+
+public class demoCode {
+	public void Nissan()
+	{
+		System.out.println("Nissan");
+	}
+
+	public void tata()
+	{
+		System.out.println("tata");
+	}
+
+	public void BMW()
+	{
+		System.out.println("BMW");
+	}
+
+	public void Audi()
+	{
+		System.out.println("Audi");
+	}
+
+}
---
+++ demoProject/src/main/java/saravanan/demoProject/deomInheritance.java
@@ -0,0 +1,11 @@
+package saravanan.demoProject;
+
+public class deomInheritance extends demoCode {
+
+	public static void main(String[] args) {
+		
+  demoCode DC = new demoCode();
+  DC.Audi();
+	}
+
+}
